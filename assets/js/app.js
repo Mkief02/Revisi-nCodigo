0fce30350bd36118429fe720b4635eb08eca0b69
Andres Perez <andresperez2098@gmail.com>
2023-11-27 14:49:12 -0600

    Primer commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,15 @@
-const baseEndpoint = 'https://api.github.com';
-const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
 
-function displayUser(username) {
+// En esta parte se puede simplificar el acceso a la API con una única variable
+// const baseEndpoint = 'https://api.github.com';
+const usersEndpoint = `https://api.github.com/users`;
+// Se agregó el caractér "." antes de "name" para referenciar dicha clase y se acmbió el nombre de las variables por unas que aportaran mayor contexto
+const name = document.querySelector('.name');
+// Se cambió el caractér "#" antes de "blog" por "." para acceder a la clase
+const blog = document.querySelector('.blog');
+const location = document.querySelector('.location');
+
+// Se modificó por una función asíncrona
+async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
   console.log(data);
